56b1aeca7770cdc74d2a5eedf8f54572cdf795b7
Sarim Ahmed <50093514+sarim2000@users.noreply.github.com>
2021-08-06 02:06:54 +0530

    added section 3

--- src/3rd-section/third.component.jsx
+++ src/3rd-section/third.component.jsx
@@ -1,5 +1,22 @@
 import "./third.styles.css";
+
 const ThirdComponent = () => {
-	return <div className="thirdcomponent"></div>;
+	return (
+		<div className="thirdcomponent">
+			<div className="rectangle1"></div>
+			<div className="rectangle2">
+				<h1>How can we help you?</h1>
+			</div>
+			<div className="tex">
+				<h1 className="list1">SAVING MONEY</h1>
+				<h1 className="list2">SAVING TIME</h1>
+				<h1 className="list3">WALK WITH CONFIDENCE</h1>
+			</div>
+
+			<div className="footer1">
+				<h1>we provide services based on your taste & with in your range</h1>
+			</div>
+		</div>
+	);
 };
 export default ThirdComponent;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ThirdComponent from "./3rd-section/third.component";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<ThirdComponent />
+		</div>
+	);
 }
 
 export default App;

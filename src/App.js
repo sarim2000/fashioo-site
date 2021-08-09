11db24943e7f2a1df3623a50bcbe20abd4567f36
I-polusta <76556107+I-polusta@users.noreply.github.com>
2021-08-09 20:35:45 +0530

    Update App.js
    app.js updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import ThirdComponent from "./3rd-section/third.component";
 import Header from "./section1/Header";
 import Section1 from "./section1/Section1";
+import Section5 from "./section5/Section5";
 function App() {
   return (
     <div className="App">
       <Header />
       <Section1 />
       <ThirdComponent />
+      <Section5 />
     </div>
   );
 }

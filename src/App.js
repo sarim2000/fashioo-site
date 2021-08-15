b83bb84a449f1ccf6e76d355a1c3ed87ebda0b24
Alan-Biju <alanbiju238@gmail.com>
2021-08-15 20:54:32 +0530

    sixth-section

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import "./App.css";
-import ThirdComponent from "./3rd-section/third.component";
+import './App.css';
+import ThirdComponent from './3rd-section/third.component';
 
-import Header from "./section1/Header";
-import Section1 from "./section1/Section1";
- import Second from './2nd-section/Second'
-import Section5 from "./section5/Section5";
+import Header from './section1/Header';
+import Section1 from './section1/Section1';
+import Second from './2nd-section/Second';
+import Section5 from './section5/Section5';
+import Sixth from './6th-section/Sixth';
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Section1 />
-    		<Second/>
-      <ThirdComponent />
-      <Section5 />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<Section1 />
+			<Second />
+			<ThirdComponent />
+			<Section5 />
+			<Sixth />
+		</div>
+	);
 }
 
 export default App;

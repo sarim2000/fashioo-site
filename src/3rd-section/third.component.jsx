45347e5891729ef59a69996ad77b347ad5b79fb9
Sarim Ahmed <50093514+sarim2000@users.noreply.github.com>
2021-08-16 01:59:51 +0530

    made some changes to css

--- src/3rd-section/third.component.jsx
+++ src/3rd-section/third.component.jsx
@@ -3,17 +3,22 @@ import "./third.styles.css";
 const ThirdComponent = () => {
 	return (
 		<div className="thirdcomponent">
-			<div className="rectangle1"></div>
+			<div className="rectangle1">
+				<div className="firstblock">
+					<h1>How can we help you?</h1>
+				</div>
+				<div className="secondblock">
+					<h1>How can we help you?</h1>
+				</div>
+			</div>
 			<div className="rectangle2">
-				<h1>How can we help you?</h1>
+				<ol>
+					<li>Saving Money</li>
+					<li>Saving Time</li>
+					<li>Walk with confidence</li>
+				</ol>
 			</div>
-			<div className="tex">
-				<h1 className="list1">SAVING MONEY</h1>
-				<h1 className="list2">SAVING TIME</h1>
-				<h1 className="list3">WALK WITH CONFIDENCE</h1>
-			</div>
-
-			<div className="footer1">
+			<div className="lastrect">
 				<h1>we provide services based on your taste & with in your range</h1>
 			</div>
 		</div>
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import ThirdComponent from './3rd-section/third.component';
+import "./App.css";
+import ThirdComponent from "./3rd-section/third.component";
 
-import Header from './section1/Header';
-import Section1 from './section1/Section1';
-import Second from './2nd-section/Second';
-import Section5 from './section5/Section5';
-import Sixth from './6th-section/Sixth';
+import Header from "./section1/Header";
+import Section1 from "./section1/Section1";
+import Second from "./2nd-section/Second";
+import Section5 from "./section5/Section5";
+import Sixth from "./6th-section/Sixth";
 function App() {
 	return (
-		<div className='App'>
+		<div className="App">
 			<Header />
 			<Section1 />
 			<Second />
